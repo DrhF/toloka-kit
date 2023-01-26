682c74c9ba4401f4edf155ed0c273c85899a0881
DrhF <mr.fedulow@gmail.com>
2023-01-26 17:37:11 +0300

    Add stubs for clearml module

--- src/integrations/clearml/__init__.pyi
+++ src/integrations/clearml/__init__.pyi
@@ -3,22 +3,11 @@ __all__ = [
     'get_clearml_dataset'
 ]
 
-import logging
-import re
 from typing import List, Optional
 
 import pandas as pd
 
 from clearml import Dataset
-import tempfile
-
-url_regex = re.compile(
-        r'^(?:http|ftp)s?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 
 def get_clearml_dataset(
@@ -39,18 +28,7 @@ def get_clearml_dataset(
         >>> get_clearml_dataset(dataset_id=dataset_id)
         ...
     """
-    if not (all([dataset_project, dataset_name]) or dataset_id):
-        raise ValueError("Either dataset_project and dataset_name or dataset_id should be provided")
-
-    dataset = Dataset.get(
-        dataset_id=dataset_id,
-        dataset_project=dataset_project,
-        dataset_name=dataset_name,
-        alias=alias
-    )
-    folder = dataset.get_local_copy()
-
-    return folder
+    ...
 
 
 def register_to_clearml(
@@ -85,34 +63,4 @@ def register_to_clearml(
         >>>                     dataset_project='ClearMLProject', dataset_name='TolokaDataset')
         ...
     """
-    if not (all([dataset_project, dataset_name]) or parent_dataset_ids):
-        raise ValueError("Either dataset_project and dataset_name or parent_dataset_id should be provided")
-
-    if not parent_dataset_ids:
-        dataset = Dataset.create(
-            dataset_project=dataset_project,
-            dataset_name=dataset_name
-        )
-    else:
-        parent = Dataset.get(dataset_id=parent_dataset_ids)
-        dataset = Dataset.create(
-            dataset_project=parent.project if not None else parent.project,
-            dataset_name=parent.name if not None else parent.name,
-            parent_datasets=[parent_dataset_ids]
-        )
-
-    toloka_csv_file = toloka_dataset_path if toloka_dataset_path is not None else f'{tempfile.gettempdir()}/toloka_ds_temp.csv'
-    toloka_df.to_csv(toloka_csv_file, index=False)
-    dataset.add_files(path=toloka_csv_file, verbose=verbose)
-
-    if external_url_columns is not None:
-        for column_name in toloka_df.columns:
-            if column_name in external_url_columns:
-                sample_value = toloka_df[column_name].iloc[0]
-                if isinstance(sample_value, str) and re.match(url_regex, sample_value) is not None:
-                    dataset.add_external_files(source_url=toloka_df[column_name].dropna().tolist(), recursive=False, verbose=verbose)
-
-    dataset.upload(verbose=verbose)
-    dataset.finalize(verbose=verbose)
-
-    logging.info(f'Dataset registered in ClearML.Dataset ID is:{dataset.id}')
+    ...
